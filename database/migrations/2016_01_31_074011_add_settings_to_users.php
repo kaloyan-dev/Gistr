895bbd76072287f8380179df6252aa8b1d6eac92
Kaloyan Ivanov <kaloyan.github@gmail.com>
2016-01-31 09:46:54 +0200

    Added user settings migration

---
+++ database/migrations/2016_01_31_074011_add_settings_to_users.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddSettingsToUsers extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->longText('settings');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
